52c0fcaa55132a2a07bbf494d08fdb31e0ae676a
Andrei Gherzan <andrei.gherzan@windriver.com>
2014-01-13 23:32:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='nodf',
+      version='1.0a',
+      scripts=['nodf'],
+      py_modules=['colorlogging'],
+      description='dotfiles manager',
+      long_description='dotfiles manager',
+      platforms=['linux'],
+      author='Andrei Gherzan',
+      author_email='andrei@gherzan.ro',
+      url='https://github.com/agherzan/nodf',
+      license='MIT',
+     )
